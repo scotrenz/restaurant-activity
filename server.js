c3943b170eade008922aec5b4ab21987671a8fd8
cvols <chris.volonnino@gmail.com>
2017-11-11 13:16:27 -0500

    backend is ready to rock

--- server.js
+++ server.js
@@ -1,30 +1,15 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-
 var app = express();
 var PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var reservations = [
-    {
-        name: '',
-        phone: '',
-        email: '',
-        unique_id: ''
-    }
-];
+var reservations = []
 
-var waitList = [
-    {
-        name: '',
-        phone: '',
-        email: '',
-        unique_id: ''
-    }
-]
+var waitList = []
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
@@ -46,6 +31,10 @@ app.post("/new/reservation", function (req, res) {
     } else {
         waitList.push(newReservation)
     }
+
+    res.send('complete')
+
+    console.log(reservations)
 })
 
 app.listen(PORT, function() {
